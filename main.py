711b15a849535bd84a9a48ddd4d3ce2c636c6d66
makewheels <makewheels@github.com>
2022-03-22 15:23:13 +0800

    init

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import requests
-import json
+
+
+# 看是否可以执行
+def can_run():
+    return True
+
+
+def run():
+    if can_run() == False:
+        return
+    # 先获取m3u8列表
+    print('')
+    content = requests.get("https://www.baidu.com").content
+
 
 if __name__ == '__main__':
-    # 先获取m3u8列表
+    run()
